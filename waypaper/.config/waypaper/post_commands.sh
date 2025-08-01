43c3b40561c3d88857ec7db70880a257a258ab9f
iamsurjog <iamsurjog@gmail.com>
2025-08-01 20:33:56 +0530

    added all files
    Former-commit-id: 0803cc0695167cfd7fc1c9e9fda0ec38b5f3c3be [formerly 9cfc3e119b4e6197b795d5c2f6c4c400038cf591] [formerly 78d46535c12db7d08c46ca0601b6f7bed6ff80a5 [formerly 931b2f6ba7a3469cd864cbeaaedaf02737296fea]] [formerly 1c729fcbcfc9bdebe92b44288167e6fb89eaacd2 [formerly 2c987251318e64b554d410173b7f436c89a3f9fc] [formerly 64cf62576df12b1779b4ab6c7e2af766adbdb0ff [formerly f531f6191bb887efc18399463c759047da8b4aa5 [formerly 71616f709980a0a00d978c3081ff9f266f5ba55c]]]]
    Former-commit-id: f37b07d50f2c702710054926127bc021865505ea [formerly 6aa48f6388cc74a5865baee06561e7dc617519a1] [formerly 0291887c553e8571a09c96aa9778df83c8d4312e [formerly 834ea6ba3724301992276789fd66eeae915700b3]]
    Former-commit-id: 39608e7bd1b086ae69570278540a0d5c3459ff69 [formerly 5e6e42ea37cfd581c4d5cecccab2a79f44781765]
    Former-commit-id: e9dfe34077814286a8df8533b8a2f012ff888126
    Former-commit-id: 4b44ed9188d2826466510a9b4af3481bdf2a0c64

---
+++ waypaper/.config/waypaper/post_commands.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Wallpaper being used is $1"
+wallust run $1
+cp $1 ~/Pictures/wallpaper.png
+killall -SIGUSR2 waybar 
+swaync-client -rs
+notify-send -i dialog-information-symbolic "Changing wallpaper and colorscheme" "Waypaper at work"
+pywalfox update
+killall swayosd-server
+spicetify apply
+sleep 0.5
+swayosd-server
