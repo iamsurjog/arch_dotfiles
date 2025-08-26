4657f5abb9190eacbc6b2927b55cd9e9c6a206a6
iamsurjog <iamsurjog@gmail.com>
2025-08-26 13:31:50 +0530

    feat: script to start tmux sessions using tmuxp

--- rofi/.config/rofi/scripts/moveto_workspace.sh
+++ rofi/.config/rofi/scripts/moveto_workspace.sh
@@ -1,13 +1,14 @@
 wkspc=$(hyprctl workspaces -j | jq -r '.[].name' | rofi -no-show-icons -keep-right -dmenu -i -p "Select Workspace:")
-if [[ "$wkspc" =~ ^[0-9]+$ ]]; then
-    hyprctl dispatch movetoworkspace "$wkspc"
-else
-    if hyprctl workspaces -j | jq -r '.[].name' | grep -qx "$wkspc"; then
-        echo "$wkspc"
+if [ -n "$wkspc" ]; then
+    if [[ "$wkspc" =~ ^[0-9]+$ ]]; then
         hyprctl dispatch movetoworkspace "$wkspc"
     else
-        hyprctl dispatch movetoworkspace "special:$wkspc"
-    # hyprctl dispatch movetoworkspace "special:$wkspc"
+        if hyprctl workspaces -j | jq -r '.[].name' | grep -qx "$wkspc"; then
+            echo "$wkspc"
+            hyprctl dispatch movetoworkspace "$wkspc"
+        else
+            hyprctl dispatch movetoworkspace "special:$wkspc"
+            # hyprctl dispatch movetoworkspace "special:$wkspc"
+        fi
     fi
 fi
-
