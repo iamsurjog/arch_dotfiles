c758d96fd88f27afb679426cbf118a62f6a9ce67
iamsurjog <iamsurjog@gmail.com>
2025-09-08 08:38:44 +0530

    feat: what?

--- rofi/.config/rofi/scripts/goto_workspaces.sh
+++ rofi/.config/rofi/scripts/goto_workspaces.sh
@@ -1,4 +1,5 @@
 wkspc=$(hyprctl workspaces -j | jq -r '.[].name' | rofi -no-show-icons -keep-right -dmenu -i -p "Select Workspace:")
+
 if [ -n "$wkspc" ]; then
     if [[ "$wkspc" =~ ^[0-9]+$ ]]; then
         hyprctl dispatch workspace "$wkspc"
