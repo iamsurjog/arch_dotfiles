51631f4a197a83f58647e9a26db6e4d29bd51aba
iamsurjog <iamsurjog@gmail.com>
2025-09-28 16:31:22 +0530

    feat: added quickshell

--- waypaper/.config/waypaper/post_commands.sh
+++ waypaper/.config/waypaper/post_commands.sh
@@ -6,10 +6,12 @@ cp $1 ~/Pictures/wallpaper.png
 killall -SIGUSR2 waybar 
 swaync-client -rs
 notify-send -i dialog-information-symbolic "Changing wallpaper and colorscheme" "Waypaper at work"
+killall quickshell
+cp ~/.cache/temp/Colours.qml ~/.config/quickshell/popout/services/Colours.qml
+rm ~/.cache/temp/Colours.qml
+quickshell --config popout
 pywalfox update
 killall swayosd-server
 spicetify apply
 sleep 0.5
-killall quickshell
-quickshell --config popout3 
 swayosd-server
