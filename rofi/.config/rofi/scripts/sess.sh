11531d699f4b3c1c94ef97aaae3c90595cb0f563
iamsurjog <iamsurjog@gmail.com>
2026-02-12 18:20:37 +0530

    feat(kitty): kitty sessions

--- rofi/.config/rofi/scripts/sess.sh
+++ rofi/.config/rofi/scripts/sess.sh
@@ -1,4 +1,4 @@
 selected_dir=$(ls $HOME/sess/ | sed 's/\.[^.]*$//'| rofi -no-show-icons -keep-right -dmenu -i -p "Select directory:")
 if [ -n "$selected_dir" ]; then
-    kitty tmuxp load "$HOME/sess/""$selected_dir"".yaml"
+    kitty --session "$HOME/sess/""$selected_dir"".kitty-session"
 fi
