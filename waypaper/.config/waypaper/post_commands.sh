f1151bc83f0a06e3d094cb7977b3ccead5c8079e
iamsurjog <iamsurjog@gmail.com>
2026-02-15 10:52:14 +0530

    feat: rm popout

--- waypaper/.config/waypaper/post_commands.sh
+++ waypaper/.config/waypaper/post_commands.sh
@@ -11,10 +11,10 @@ fi
 killall -SIGUSR2 waybar 
 swaync-client -rs
 notify-send -i color "Changing wallpaper and colorscheme" "Waypaper at work" -a "Waypaper"
-killall quickshell
+# killall quickshell
 cp ~/.cache/temp/Colours.qml ~/.config/quickshell/popout/services/Colours.qml
 rm ~/.cache/temp/Colours.qml
-quickshell -c popout
+# quickshell -c popout
 # quickshell -c sidebar -n
 pywalfox update
 killall swayosd-server
@@ -25,4 +25,4 @@ fi
 sleep 0.5
 # quickshell -c sidebar
 swayosd-server
-quickshell -c sidebar
+# quickshell -c sidebar
